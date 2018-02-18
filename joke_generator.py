7fe4af34ceb8267ac9454d7d32a0896a83ad9033
arduino5144 <Leonard@tussilago.eu>
2018-02-18 13:41:03 +0700

    Update joke_generator.py

--- joke_generator.py
+++ joke_generator.py
@@ -1,5 +1,25 @@
 import random
+import time
 
-joketype=random.randint(1,2)
+joketype=random.randint(1,3)
 
-if joketype=1
+if joketype==1:
+    jokeid1=random.randint(1,6)
+    if jokeid1==1
+        rid="What’s the easiest way to double your money?"
+        ans="Put it in front of a mirror."
+     elif jokeid1==2:
+         rid="What occurs twice in a week, once in a year but never in a day?"
+         ans="The letter E."
+     elif jokeid1==3
+         rid="What 4 days of the week start with the letter ‘t’?" 
+         ans="Tuesday, Thursday, today and tomorrow."   
+     elif jokeid1==4:
+         rid="What is the hardest part about skydiving?"
+         ans="The ground!"     
+     elif jokeid1==5:
+         rid="What vehicle is spelled the same backwards and forwards?"
+         ans="Racecar"
+     print(rid) 
+     time.sleep(5)
+     print(ans)
