4e77d2128da7c235d73275e90ceafe80407c09d7
BuildTools <unconfigured@null.spigotmc.org>
2018-03-11 20:27:11 +0700

    Added knock knock jokes

--- joke_generator.py
+++ joke_generator.py
@@ -7,19 +7,41 @@ if joketype==1:
     jokeid1=random.randint(1,6)
     if jokeid1==1
         rid="What’s the easiest way to double your money?"
-        ans="Put it in front of a mirror."
+        ridans="Put it in front of a mirror."
      elif jokeid1==2:
          rid="What occurs twice in a week, once in a year but never in a day?"
-         ans="The letter E."
+         ridans="The letter E."
      elif jokeid1==3
          rid="What 4 days of the week start with the letter ‘t’?" 
-         ans="Tuesday, Thursday, today and tomorrow."   
+         ridans="Tuesday, Thursday, today and tomorrow."   
      elif jokeid1==4:
          rid="What is the hardest part about skydiving?"
-         ans="The ground!"     
+         ridans="The ground!"     
      elif jokeid1==5:
          rid="What vehicle is spelled the same backwards and forwards?"
-         ans="Racecar"
+         ridans="Racecar"
      print(rid) 
      time.sleep(6)
-     print(ans)
+     print(ridans)
+elif joketype==2
+    jokeid2=random.randint(1,21)
+    
+    if jokeid==1
+        name="Canoe"
+        who="Canoe who?"
+        knockknockans="Canoe come out and play with me today?"
+    elif jokeid==2
+        name="Canoe"
+        who="Canoe who?"
+        knockknockans="Canoe come out and play with me today?"  
+
+
+    print("Knock knock")    
+    time.sleep(2)
+    print("Who's there?")
+    time.sleep(3)
+    print(name)
+    time.sleep(wordtime)
+    print(who)
+    time.sleep(10)
+    print(knockknockans)
